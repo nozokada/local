d1df03ed9647625b10f7a53784f9d0f4764fa255
Nozomi Okada <nokada@proofpoint.com>
2019-12-15 23:39:58 -0700

    Adding price and description to ItemVC

--- local/Controller/ItemVC.swift
+++ local/Controller/ItemVC.swift
@@ -12,19 +12,27 @@ class ItemVC: UIViewController {
 
     @IBOutlet weak var itemImageView: UIImageView!
     @IBOutlet weak var itemTitleLabel: UILabel!
+    @IBOutlet weak var itemPriceLabel: UILabel!
+    @IBOutlet weak var itemDescriptionLabel: UILabel!
     
     var itemImage: UIImage?
     var itemTitle: String?
+    var itemPrice: String?
+    var itemDescription: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         itemImageView.image = itemImage
         itemTitleLabel.text = itemTitle
+        itemPriceLabel.text = itemPrice
+        itemDescriptionLabel.text = itemDescription
     }
     
     func initItem(item: Item) {
         itemImage = UIImage(named: item.imageName)
         itemTitle = item.title
+        itemPrice = item.price
+        itemDescription = item.description
     }
 }
