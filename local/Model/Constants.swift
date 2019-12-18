21b2b4256a91982ede813c7b3b8cd6a9c8fffe9e
Nozomi Okada <nokada@proofpoint.com>
2019-12-18 00:16:32 -0700

    Adding app icon and logo

--- local/Model/Constants.swift
+++ local/Model/Constants.swift
@@ -8,9 +8,7 @@
 
 import UIKit
 
-let MAIN_COLOR = #colorLiteral(red: 0.1128538623, green: 0.2610458136, blue: 0.3198455572, alpha: 1)
-let BACK_GROUND_COLOR = #colorLiteral(red: 0.5456358194, green: 0.5624058247, blue: 0.4958024621, alpha: 1)
-let BOTTOM_BAR_COLOR = #colorLiteral(red: 0.9625054002, green: 0.9735464454, blue: 0.9380173087, alpha: 1)
+let MAIN_COLOR = #colorLiteral(red: 0.09220222384, green: 0.2645914555, blue: 0.3533145189, alpha: 1)
 let MAIN_FONT_REGULAR = "AvenirNext-Regular"
 let MAIN_FONT_MEDIUM = "AvenirNext-Medium"
 let MAIN_FONT_BOLD = "AvenirNext-Bold"
